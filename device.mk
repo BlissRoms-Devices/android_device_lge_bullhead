a207e62ffb31e3e18f0c8e3b3fa52cefd48ffcb6
beanstown106 <nbenis106@gmail.com>
2015-11-07 19:25:11 -0500

    bullhead: Add old-apns-conf.xml * not entirely sure its needed but factory image has it maybe its a fallback
    Change-Id: Iab60579c204a6d32d901f6332f464c6d9b8642b7

--- device.mk
+++ device.mk
@@ -451,6 +451,10 @@ PRODUCT_PACKAGES += \
     power.bullhead \
     thermal.bullhead
 
+# old-apns.conf
+PRODUCT_COPY_FILES += \
+    device/lge/bullhead/old-apns-conf.xml:system/etc/old-apns-conf.xml
+
 # Modem debugger/misc
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 ifeq (,$(filter aosp_bullhead, $(TARGET_PRODUCT)))
