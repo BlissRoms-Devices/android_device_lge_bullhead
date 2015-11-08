b371d051d73161de6e9bbdc38bdca3a5556a9828
beanstown106 <nbenis106@gmail.com>
2015-11-07 21:57:24 -0500

    bullhead: build kernel inline
    Change-Id: I4e8b23d9575067aee3bd77802bb0dc113a7aab10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,13 @@ ENABLE_CPUSETS := true
 
 TARGET_NO_BOOTLOADER := true
 
+# Inline kernel building
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_SOURCE := kernel/lge/bullhead
+TARGET_KERNEL_CONFIG := pure_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
