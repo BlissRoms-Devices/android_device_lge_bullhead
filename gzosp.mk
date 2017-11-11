bd62266830752ccdc3058170ce6e33ff477b0a45
sparx <alliancesparx@gmail.com>
2017-11-11 14:52:07 +0000

    Update build fingerprint

--- gzosp.mk
+++ gzosp.mk
@@ -17,8 +17,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:8.0.0/OPR4.170623.009/4302492:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR4.170623.009 4302492 release-keys"
+    BUILD_FINGERPRINT=google/bullhead/bullhead:8.0.0/OPR6.170623.023/4409485:user/release-keys \
+    PRIVATE_BUILD_DESC="bullhead-user 8.0.0 OPR6.170623.023 4409485 release-keys"
     
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
