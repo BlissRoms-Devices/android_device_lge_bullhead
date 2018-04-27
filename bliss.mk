7d55420fc860c17af15457e36640f9cb16cfd34a
Eric Park <ideamaneric@gmail.com>
2018-04-27 00:05:31 -0400

    Blissify GZOSP trees
    Kudos to the GZOSP developers and maintainers. This is my desperate
    attempts to get Bliss on the Nexus 5X. If this doesn't work I don't
    know what will

--- bliss.mk
+++ bliss.mk
@@ -2,14 +2,14 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common GZOSP stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := gzosp_bullhead
+PRODUCT_NAME := bliss_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
@@ -19,7 +19,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys \
     PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
-    
+
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.control_privapp_permissions=permissive \
