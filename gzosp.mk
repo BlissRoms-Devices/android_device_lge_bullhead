6c58b6a2db06e942bc47663fd6263e28cd1d9239
allianceSparx <alliancesparx@gmail.com>
2018-05-12 10:08:49 +0100

    Update build fingerprint to OPM4.171019.016.A1 May 2018

--- gzosp.mk
+++ gzosp.mk
@@ -17,8 +17,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT=google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM4.171019.016.A1 4720815 release-keys"
+
+BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM4.171019.016.A1/4720815:user/release-keys
     
 # Google property overides
 PRODUCT_PROPERTY_OVERRIDES += \
