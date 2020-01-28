b9b5a0e57a244f964f00a57b1e63c44b93d95914
Jackeagle <jackeagle102@gmail.com>
2020-01-28 14:49:37 +0100

    Blissify bullhead
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,7 +15,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/gzosp_bullhead.mk
+    $(LOCAL_DIR)/bliss_bullhead.mk
 
 COMMON_LUNCH_CHOICES := \
-    gzosp_bullhead-userdebug
+    bliss_bullhead-userdebug
--- bliss_bullhead.mk
+++ bliss_bullhead.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common GZOSP stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := gzosp_bullhead
+PRODUCT_NAME := bliss_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
